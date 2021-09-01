22577d82c66d4e51ce4e375984925f3683a12d53
Gin-is-not-real <ninapariat@live.fr>
2021-09-01 16:54:30 +0200

    php request

--- php/operations.php
+++ php/operations.php
@@ -1,12 +1,37 @@
 <?php 
+$hostname = 'localhost';
+$basename = 'projet_budget';
+$username = 'admin';
+$password = 'admin';
+$tablename = 'operations';
+$pdo = newPdo($hostname, $basename, $username, $password, $tablename);
+
 var_dump($_POST);
 
-
-function pdoConnect() {
-    
+function newPdo($hostname, $basename, $username, $password, $tablename) {
+    try {
+        $pdo = new PDO("mysql:host=" . $hostname . ";dbname=" . $basename . ";charset=utf8", $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        return $pdo;
+    }
+    catch(Exception $e) {
+        die('ERROR: ' . $e->getMessage());
+    }
 }
 
-function addOperation($id, $line) {
-    
+
+function addOperation($pdo, $id, $line) {
+    try {
+        $req = $pdo->prepare("INSERT INTO operations (line_id, expense, income, label) VALUES (:line_id, :expense, :income, :label)");
+        $affectedLines = $req->execute(array(
+            'line_id' => $id,
+            'expense' => $line['expense'],
+            'income' => $line['income'],
+            'label' => $line['label']
+        ));
+        return $req;
+    }
+    catch(Exception $e) {
+        die('ERROR: ' . $e->getMessage());
+    }
 }
 
