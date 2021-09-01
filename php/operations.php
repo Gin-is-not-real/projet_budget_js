b85cfeec339a18d09fd680f0a730f95b7ede6370
Gin-is-not-real <ninapariat@live.fr>
2021-09-01 16:28:39 +0200

    add php folder

---
+++ php/operations.php
@@ -0,0 +1,12 @@
+<?php 
+var_dump($_POST);
+
+
+function pdoConnect() {
+    
+}
+
+function addOperation($id, $line) {
+    
+}
+
--- scripts/ajax.js
+++ scripts/ajax.js
@@ -25,4 +25,43 @@ function getHTTPObject() {
     return xmlhttp;
 }
 
-//creer requete POST
+window.onload = function() {
+    //on associe la fonction verifId à l'événénement onsubmit du formulaire
+    document.getElementById('form-new-operation').onsubmit = verifId;
+}
+
+function verifId() {
+    return !sendData(
+        'POST',
+        'operations-xml.php',
+        'xmlhttp = 1&' + 'expense=' + inpExpense.value + '&' + 'income' + inpIncome.value
+    );
+}
+
+// * Envoie des données à l'aide d'XmlHttpRequest?
+// * @param string methode d'envoi ['GET'|'POST']
+// * @param string url
+// * @param string données à envoyer sous la forme var1=value1&var2=value2...
+function sendData(method, url, data) {
+    let xmlhttp = getHTTPObject();
+
+    if(!xmlhttp) {
+        return false;
+    }
+
+    if(method == 'GET') {
+        if(data == 'null') {
+            xmlhttp.open('GET', url, true);//ouverture asynchrone
+        }
+        else {
+            xmlhttp.open('GET', url + '?' + data, true);
+        }
+        xmlhttp.send(null);
+    }
+    else if(mathod == "POST") {
+        xmlhttp.open('POST', url, true);
+        xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xmlhttp.send(data);
+    }
+    return true;
+}
