2201c258c7247ee753a38f56e1b6ff0095172778
Gin-is-not-real <ninapariat@live.fr>
2021-09-02 11:59:54 +0200

    succeded for pass POST variables on request

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -2,7 +2,7 @@ let httpRequest = new XMLHttpRequest();
 
 let textOfPhp = '';
 
-function testRequest() {
+function testRequest(url) {
     console.log(httpRequest);
     //on defini la fonction qui traitera la reponse du serveur 
     // httpRequest.onreadystatechange = myFunction;
@@ -20,10 +20,11 @@ function testRequest() {
                 //pour une reponse texte:
                 // console.log('responseText : ', httpRequest.responseText);
 
-                //pour une reponse xml (ne fonctionne pas..)
+                //pour une reponse xml 
                 let xmldoc = httpRequest.responseXML;
                 let root_node = xmldoc.getElementsByTagName('root').item(0);
-                alert(root_node.firstChild.data);
+                let data = root_node.firstChild.data;
+                alert(data);
                 console.log('responseXML : ', httpRequest.responseXML);
             }
             else {
@@ -36,10 +37,40 @@ function testRequest() {
     };
     //on lance la requete
     // httpRequest.open('GET', 'ajax/ajax.php?get=youhou', true);
-    httpRequest.open('GET', 'ajax/ajax.xml', true);
-
+    // httpRequest.open('GET', 'ajax/ajax.xml', true);
+    httpRequest.open('GET', url, true);
     httpRequest.send();
 }
-testRequest();
+// testRequest('ajax/ajax.xml');
 
 
+function sendRequest(url, variable) {
+    httpRequest.onreadystatechange = alertContents;
+    //pass un get + u post
+    // httpRequest.open('POST', url + '?get=' + encodeURIComponent(variable), true);
+
+    httpRequest.open('POST', url, true);
+
+    
+    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    httpRequest.send('variable=' + encodeURIComponent(variable));
+}
+
+function alertContents() {
+        //traitement de la reponse
+        console.log('---------> readyState: ', this.readyState, 'status: ', this.status);
+
+        if(httpRequest.readyState === XMLHttpRequest.DONE) {
+            if(httpRequest.status === 200) {
+                let response = JSON.parse(httpRequest.responseText);
+                alert(response.computedString);
+            }
+            else {
+                console.log('probleme avec la requete', httpRequest.status);
+            }
+        }
+        else {
+            console.log('---> pas encore pret');
+        }
+}
+sendRequest('ajax/ajax.php', 'et voici ma variable');
--- ajax/ajax.php
+++ ajax/ajax.php
@@ -1,4 +1,9 @@
 <?php 
-echo 'je suis la page ajax.php ' . $_GET['get'];
+// echo 'je suis la page ajax.php ' . $_GET['get'];
+
+$variable = (isset($_POST['variable'])) ? $_POST['variable'] : 'void';
+$computedString = 'Bonjour, ' . $variable . ' !';
+$array = ['variable' => $variable, 'computedString' => $computedString];
+echo json_encode($array);
 ?>
 
