b5b11d43b31845d6eb8b8490c645c1ba4bb5e2d1
qbaccon <qbaccon@hotmail.com>
2021-09-02 16:50:48 +0200

    update style

--- scripts/script.js
+++ scripts/script.js
@@ -22,45 +22,48 @@ tabTransfers.push({'label': 'Expense', 'expense': 10, 'income': 0});
 tabTransfers.push({'label': 'Income', 'expense': 0, 'income': 50});
 tabTransfers.push({'label': 'Expense', 'expense': 30, 'income': 0});
 tabTransfers.push({'label': 'Expense', 'expense': 25, 'income': 0});
-// tabTransfers.push({'label': 'test', 'expense': 50, 'income': 0});
-// tabTransfers.push({'label': 'test', 'expense': 40, 'income': 0});
-// tabTransfers.push({'label': 'test', 'expense': 0, 'income': 30});
-// tabTransfers.push({'label': 'test', 'expense': 10, 'income': 0});
-// tabTransfers.push({'label': 'test', 'expense': 0, 'income': 70});
-// tabTransfers.push({'label': 'test', 'expense': 0, 'income': 60});
-// tabTransfers.push({'label': 'test', 'expense': 0, 'income': 45});
+tabTransfers.push({'label': 'Expense', 'expense': 50, 'income': 0});
+tabTransfers.push({'label': 'Expense', 'expense': 40, 'income': 0});
+tabTransfers.push({'label': 'Income', 'expense': 0, 'income': 30});
+tabTransfers.push({'label': 'Expense', 'expense': 10, 'income': 0});
+tabTransfers.push({'label': 'Income', 'expense': 0, 'income': 70});
+tabTransfers.push({'label': 'Income', 'expense': 0, 'income': 60});
+tabTransfers.push({'label': 'Income', 'expense': 0, 'income': 45});
 sum_tab = get_sum();
 sum_dspl(sum_tab);
 
 function addExpense() {
-    let value = inpExpense.value;
-    let label = inpLabel.value || 'Expense';
-    let lineObject = {'label': label, 'expense': Number(value), 'income': 0};
+	if (inpExpense.value != '')
+    {
+		let value = inpExpense.value;
+    	let label = inpLabel.value || 'Expense';
+    	let lineObject = {'label': label, 'expense': Number(value), 'income': 0};
 
-    tabTransfers.push(lineObject);
+   		tabTransfers.push(lineObject);
 
-    createLine(tabTransfers.length - 1, lineObject);
+		createLine(tabTransfers.length - 1, lineObject);
 
-	sum_tab = get_sum();
-	sum_dspl(sum_tab);
-
+		sum_tab = get_sum();
+		sum_dspl(sum_tab);
+	}
     inpExpense.value = '';
 }
 
 function addIncome() {
-    let value = inpIncome.value;
-    let label = inpLabel.value || 'Income';
-    let lineObject = {'label': label, 'expense': 0, 'income': Number(value)};
-    console.log(lineObject);
-    tabTransfers.push(lineObject);
+	if (inpIncome.value != '')
+    {
+		let value = inpIncome.value;
+		let label = inpLabel.value || 'Income';
+		let lineObject = {'label': label, 'expense': 0, 'income': Number(value)};
 
-    createLine(tabTransfers.length - 1, lineObject);
+		tabTransfers.push(lineObject);
 
-	sum_tab = get_sum();
-	sum_dspl(sum_tab);
+		createLine(tabTransfers.length - 1, lineObject);
 
+		sum_tab = get_sum();
+		sum_dspl(sum_tab);
+	}
     inpIncome.value = '';
-
 }
 
 
